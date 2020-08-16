c10da312d8f1b7a672fa2ee78be40cdfa40449d6
Fanilo Andrianasolo <andfanilo@users.noreply.github.com>
2020-08-16 22:36:01 +0200

    Revert "Merge Vue 3 branch (#1)"
    This reverts commit dde813765e7e6c86669ae255089f80f2fbd1a53f.

--- my_component/frontend/src/App.vue
+++ my_component/frontend/src/App.vue
@@ -1,6 +1,6 @@
 <!-- 
   We bootstrap our Component to Streamlit with our scoped slot in the top-level App.
-  This is where scoped slot passes Streamlit `args` data from itself to children MyComponent.
+  This is where scoped slot passes Streamlit ;`args` data from itself to children MyComponent.
   You should not have to edit this, but are free to do so :)
 -->
 <template>
@@ -11,30 +11,27 @@
   </div>
 </template>
 
-<script lang="ts">
-import { defineComponent } from "vue"
-import MyComponent from "./MyComponent.vue"
+<script>
+import MyComponent from "./MyComponent.vue";
 
 // "withStreamlitConnection" is a scoped slot. It bootstraps the
 // connection between your component and the Streamlit app, and handles
 // passing arguments from Python -> Component.
 //
 // You don't need to edit withStreamlitConnection (but you're welcome to!).
-import WithStreamlitConnection from "./streamlit/WithStreamlitConnection.vue"
+import WithStreamlitConnection from "./streamlit/WithStreamlitConnection.vue";
 
-export default defineComponent({
+export default {
   name: "App",
-  components: {
-    MyComponent,
-    WithStreamlitConnection,
-  },
-})
+  components: { MyComponent, WithStreamlitConnection }
+};
 </script>
 
 <style>
 body {
   margin: 0;
 }
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
--- my_component/frontend/src/MyComponent.vue
+++ my_component/frontend/src/MyComponent.vue
@@ -1,30 +1,37 @@
 <template>
-  <span>
-    Hello, {{ args.name }}! &nbsp;
-    <button @click="onClicked">Click Me!</button>
-  </span>
+  <div class="wrapper">
+    <!-- 
+      Show a button and some text.
+      When the button is clicked, we'll increment our "numClicks" state
+      variable, and send its new value back to Streamlit, where it'll
+      be available to the Python program.
+    -->
+    <span>
+      Hello, {{args.name}}! &nbsp;
+      <button v-on:click="onClicked()">Click Me!</button>
+    </span>
+  </div>
 </template>
 
 <script>
-import { ref } from "vue"
-import { Streamlit, useStreamlit } from "./streamlit"
+import { Streamlit } from "./streamlit";
 
 export default {
   name: "MyComponent",
-  props: ["args"], // Arguments that are passed to the plugin in Python are accessible in prop "args"
-  setup() {
-    useStreamlit() // lifecycle hooks for automatic Streamlit resize
-
-    const numClicks = ref(0)
-    const onClicked = () => {
-      numClicks.value++
-      Streamlit.setComponentValue(numClicks.value)
-    }
-
+  props: ["args"], // Arguments that are passed to the plugin in Python are accessible in props `args`. Here, we access the "name" arg.
+  data() {
     return {
-      numClicks,
-      onClicked,
-    }
+      numClicks: 0
+    };
   },
-}
+  methods: {
+    /** Click handler for our "Click Me!" button. */
+    onClicked: function() {
+      // Increment this.numClicks, and pass the new value back to
+      // Streamlit via `Streamlit.setComponentValue`.
+      this.numClicks++;
+      Streamlit.setComponentValue(this.numClicks);
+    }
+  }
+};
 </script>
